b4b5cc6f207faa8b3d4b8df7fb89044a7bedceec
catherin <catherinr24@gmail.com>
2024-03-13 17:55:53 -0300

    comenzamos la funcion filtrado

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,9 +1,10 @@
 // Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
-
-export const example = () => {
-  return 'example';
+export const filterData = (data, filterBy, value) => {
+  
+  return data.filter(singer => singer.facts[filterBy] === value);
+ 
 };
 
-export const anotherExample = () => {
-  return [];
-};
+// export const anotherExample = () => {
+//   return [];
+// };

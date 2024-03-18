0787eb27ca5c027df08c088fdad13cc92af26a71
deniisolo <deniisolo@outlook.es>
2024-03-18 11:59:52 -0500

    función estadistica done

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { filterData } from "./dataFunctions.js";
+import { sortData } from "./dataFunctions.js";
+import { computeStats } from "./dataFunctions.js";
 import { renderItems } from "./view.js";
 
 import data from "./data/dataset.js";
@@ -10,6 +12,9 @@ renderItems(data);
 //variables
 const container = document.querySelector("#root");
 const mainGenre = document.querySelector("#mainGenre");
+const compute = document.getElementById("compute");
+const changeCompute = document.getElementById("conteinerCompute");
+const sort = document.querySelector("#sort");
 
 container.appendChild(renderItems(data));
 
@@ -17,5 +22,25 @@ container.appendChild(renderItems(data));
 mainGenre.addEventListener("change", (e) => {
   const optionValue = e.target.value;
   const filterResult = filterData(data, "mainGenre", optionValue);
-  container.appendChild(renderItems(filterResult))
+  container.appendChild(renderItems(filterResult));
+
+  sort.addEventListener("change", (e) => {
+    const optionSort = e.target.value;
+    const sortResult = sortData(filterResult, "yearOfBirth", optionSort);
+    container.appendChild(renderItems(sortResult));
+  });
+});
+sort.addEventListener("change", (e) => {
+  const optionSort = e.target.value;
+  const sortResult = sortData(data, "yearOfBirth", optionSort);
+  container.appendChild(renderItems(sortResult));
+});
+
+compute.addEventListener("click", function () {
+  const cumputeResult = computeStats(data);
+  changeCompute.innerHTML =
+    "Sabias que el:   " +
+    cumputeResult +
+    "%" +
+    " de cantantes nacieron entre 1970 y 1990";
 });

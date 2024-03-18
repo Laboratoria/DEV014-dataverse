129fa283b3ba7cacca9d434fc16e149e88d4b503
deniisolo <deniisolo@outlook.es>
2024-03-18 17:57:57 -0500

    feat: agregamos prueba unitaria de compute

--- test/dataFunctions.spec.js
+++ test/dataFunctions.spec.js
@@ -1,18 +1,31 @@
-import { example, anotherExample } from '../src/dataFunctions.js';
-// import { data as fakeData } from './data.js';
+import { filterData, sortData, computeStats } from "../src/dataFunctions.js";
 
-// console.log(fakeData);
-
-describe('example', () => {
-
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+import { data as fakeData } from "./data.js";
+//test
+describe("Check that filter function works", () => {
+  it("returns All of singer who main genre is Pop", () => {
+    expect(filterData(fakeData, "mainGenre", "Pop")).toHaveLength(3);
+  });
+  it("returns All of singer who main genre is Rock", () => {
+    expect(filterData(fakeData, "mainGenre", "Rock")).toHaveLength(1);
+  });
+  it("returns All of singer who main genre is Latino", () => {
+    expect(filterData(fakeData, "mainGenre", "Latino")).toHaveLength(1);
   });
 });
 
-describe('anotherExample', () => {
-
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+describe("Check that SortData function works",() => {
+  it("Return All singer Sorted from filter function increasingly", () => {
+    const sortedData = sortData(fakeData, "yearOfBirth", "asc");
+    expect(sortedData[0].facts.yearOfBirth).toEqual(1975);
   });
-});
+  it("Return All singer Sorted from filter function descendently", () => {
+    const sortedData = sortData(fakeData, "yearOfBirth", "desc");
+    expect(sortedData[0].facts.yearOfBirth).toEqual(1994);
+  });
+}); 
+describe("Check that computeStats function works",() => {
+  it("Return the statistics on singers born between 1970 and 1990.", () => {
+    expect(computeStats(fakeData)).toBe("80.00");
+  });
+}); 

8aea7c57514d33cfbe0a1cc4cdbec08229235735
catherin <catherinr24@gmail.com>
2024-03-19 12:24:36 -0300

    arreglando itemscope itemprop y el otro microdato

--- src/view.js
+++ src/view.js
@@ -10,14 +10,14 @@ export const renderItems = (data) => {
     const liSinger = document.createElement("li");
     liSinger.classList.add("styleLi");
     const dlSinger = document.createElement("dl");
-    liSinger.setAttribute("itemscope", "");
-    liSinger.setAttribute("itemtype", "singers");
+    dlSinger.setAttribute("itemscope", "");
+    dlSinger.setAttribute("itemtype", "singers");
 
     dlSinger.innerHTML = `
       <dt class="nameSinger">${singer.name}</dt>
       <img src="${singer.imageUrl}">
       <dt itemprop="shortDescription" class="shortDescription" >${singer.shortDescription}</dt>
-      <dt itemprop="yearOfBirth" class="yearOfBirth"> <span>Año de Nacimiento:</span> ${singer.facts.yearOfBirth}</dt>
+      <dt itemprop="sort" class="yearOfBirth"> <span>Año de Nacimiento:</span> ${singer.facts.yearOfBirth}</dt>
       <dt itemprop="placeOfBirth" class="placeOfBirth"> <span>Lugar de Nacimiento:</span> ${singer.facts.placeOfBirth}</dt>
       <dt itemprop="mainGenre" class="mainGenre"> <span>Género:</span>${singer.facts.mainGenre}</dt>
     `;
